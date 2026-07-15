d9dd6b71ca5baecdf5af9820563411fe5613fcf5
Akor Akazue <a3isalink@gmail.com>
2026-07-15 13:33:50 +0100

    Fix compile error: simplify custom LauncherActivity, remove drawable/splash entirely

--- android/app/src/main/java/com/lifelinepro/app/Application.java
+++ android/app/src/main/java/com/lifelinepro/app/Application.java
@@ -1,22 +1,18 @@
 package com.lifelinepro.app;
 
 import android.net.Uri;
+import android.os.Bundle;
 import com.google.androidbrowserhelper.trusted.LauncherActivity;
 
 /**
- * Custom LauncherActivity that disables the splash screen strategy
- * to avoid the "width and height must be > 0" crash when the library
- * tries to convert a vector/layer-list drawable to a bitmap.
+ * Custom LauncherActivity that provides the launch URL.
+ * The splash screen crash is avoided by not including
+ * SPLASH_IMAGE_DRAWABLE meta-data in the manifest.
  */
 public class Application extends LauncherActivity {
 
     @Override
     protected Uri getLaunchingUrl() {
         return Uri.parse("https://lifeline-pro-frontend.vercel.app/");
-    }
-
-    @Override
-    protected boolean useSplashScreen() {
-        return false;
     }
 }
